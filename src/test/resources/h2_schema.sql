5d5202ba807aff01d3efbeabc5cc1a2d568bb467
Edward Wu <edwardwu@xrex.io>
2023-08-08 19:10:35 +0800

    Initial commit

---
+++ src/test/resources/h2_schema.sql
@@ -0,0 +1,6 @@
+id bigint(20) not null PK
+`user_id` varchar(36) NOT NULL,
+name varchar(50) not null comment 'last_name + first_name'
+first_name varchar(30) not null
+last_name varchar(20) not null
+identity_number(20) not null
